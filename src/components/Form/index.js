262e1c275aab97cf9c99d28674218aab487632c4
Diego Armando Payán López <22713458+DiegoPayan@users.noreply.github.com>
2023-03-08 13:50:21 -0700

    Update index.js

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"; // No es necesario importar React aquí
 import { Divider, TextField } from "@material-ui/core";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
@@ -7,9 +7,9 @@ import Button from "@material-ui/core/Button";
 import DeleteIcon from "@material-ui/icons/Delete";
 import IconButton from "@material-ui/core/IconButton";
 import axios from "axios";
-import "./style.css";
+import "./style.css"; // Te recomiendo utilizar alguna librería como tailwind o bulmacss
 
-const Form = (urlPokemones) => {
+const Form = (urlPokemones) => { // No hay motivos para pasar la url como prop si a fin de cuentas es una constante que no varía
   const [pokemonesTemporales, setPokemonesTemporales] = useState([]);
   const [pokemonesArray, setPokemonesArray] = useState([]);
   const [searchPokemon, setSearchPokemon] = useState("");
@@ -17,10 +17,10 @@ const Form = (urlPokemones) => {
   const [openDialogPok, setOpenDialogPok] = useState(false);
   const [indicePokemon, setIndicePokemon] = useState("");
 
-  useEffect(() => {
+  useEffect(() => { // En general creo que toda la estructura de este useEffect se puede mejorar, hay formas de hacer llamadas con async/await dentro de un useEffect.
     let pokemonesInfo = [];
     axios.get(urlPokemones.lista).then((response) => {
-      let pokemones = response.data.results;
+      let pokemones = response.data.results; // Esta variable la podrías declarar como const, no la cambias después, también podrías haber echo `const { results: pokemones } = data.results`
       pokemones.map((val) => {
         let pokemon = {};
         pokemon.nombre = val.name;
@@ -30,11 +30,11 @@ const Form = (urlPokemones) => {
           pokemon.abilities = infoPokemon.abilities;
           pokemon.peso = infoPokemon.weight;
           pokemonesInfo.push(pokemon);
-          setPokemonesArray(pokemonesInfo);
+          setPokemonesArray(pokemonesInfo); // Estás haciendo un setState dentro de un map dentro de un useEffect, eso puede traer problemas de rendimiento.
         });
       });
     });
-  }, []);
+  }, []); // En general y como te comentaba al principio, yo creo que el approach ideal es wrappear todo el contenido del useEffect en una funcion async que devuelva el array con pokemonesInfo y ya con ese response hacer el setState une vez en lugar de por cada pokemon de la lista.
 
   useEffect(() => {
     if (banderaPokemon) {
@@ -46,16 +46,16 @@ const Form = (urlPokemones) => {
     }
   }, [pokemonesArray, banderaPokemon]);
 
-  const eliminarPokemon = (indice) => {
+  const eliminarPokemon = (indice) => { // Si como te comento en otra parte, utilizaras un id, podrías/tendrías que hacer un filter en lugar de un splice `pokemonesArray.filter(p => p.id !== id)`
     let pokemonDelete = pokemonesArray;
     pokemonDelete.splice(indice, 1);
     setPokemonesArray(pokemonDelete);
     setPokemonesTemporales(pokemonDelete);
   };
 
-  const onChangeSearchPokemon = (e) => {
+  const onChangeSearchPokemon = (e) => { // Aquí podrías haber hecho una deconstrucción `const onChangeSearchPokemon = ({ target: { value } }) =>`
     if (e.target.value !== "") {
-      setSearchPokemon(e.target.value);
+      setSearchPokemon(e.target.value); // Este set es igual al que haces abajo, podrías ahorrarte unas lineas si haces el setSearchPokemon fuera de los if y un solo `if (e.target.value === "")` con el setPokemonesArray dentro
     } else if (e.target.value === "") {
       setSearchPokemon(e.target.value);
       setPokemonesArray(pokemonesTemporales);
@@ -67,21 +67,21 @@ const Form = (urlPokemones) => {
     setIndicePokemon(indice);
   };
 
-  const handleCloseDialogPokemon = () => {
+  const handleCloseDialogPokemon = () => { // Puedes eliminar esta función si haces el setState en donde estás mandando a llamarla
     setOpenDialogPok(false);
   };
 
   const pokemonesGrid = () => {
-    return pokemonesArray.map((val, indice) => {
+    return pokemonesArray.map((val, indice) => { // No se ve como buena práctica el utilizar el indice de un map para dictaminar el identificador de un elemento, yo te aconsejo tener algo como `val.id` o similar.
       let pokemonKey = val.nombre + String(indice);
       return (
-        <div className="gridPokemon" key={pokemonKey}>
+        <div className="gridPokemon" key={pokemonKey}> // Lo mismo para los key, podrías usar el `val.id`
           <div className="alignText itemPokemonGrid" key={val.nombre}>
             <span
               className="cursorPointer"
               onClick={() => handleOpenDialogPokemon(indice)}
             >
-              {val.nombre}
+              {val.nombre} // Para no estar haciendo `val.nombre`, `val.id`, `val.imagen` puedes hacer una declaración al principio como `const { id, imagen, nombre } = val;`
             </span>
           </div>
           <div className=" alignText itemPokemonGrid">
